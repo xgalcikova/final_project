7976e1b851b41cd2e441f3f33eb09bc6f8c5d225
Kristina Fedorova <xfedorovak@stuba.sk>
2019-12-17 10:41:45 +0100

    major[mfiles]: changed

--- analyzuj_vlastnosti.m
+++ analyzuj_vlastnosti.m
@@ -1,7 +1,9 @@
 function analyzuj_vlastnosti(cit, men)
     if length(cit)>length(men)
         disp('System nie je fyzikalne realizovatelny.')
+    elseif length(cit)==length(men)
+        disp('System je rydzi.')
     else
-        disp('System je fyzikalne realizovatelny.')
+        disp('System je striktne rydzi.')
     end        
 end
--- testuj.m
+++ testuj.m
@@ -1,9 +1,13 @@
 clc, clear, close all;
-% fyzikalna realizovatelnost
-cit1 = [9];
-men1 = [1 2 3];
-analyzuj_vlastnosti(cit1, men1)
 % fyzikalna nerealizovatelnost
-cit2 = [9 9 9];
-men2 = [1 2];
+cit1 = [9 9 9];
+men1 = [1 2];
+analyzuj_vlastnosti(cit1, men1)
+% striktne rydzi
+cit2 = [9];
+men2 = [1 2 3];
 analyzuj_vlastnosti(cit2, men2)
+% rydzi
+cit3 = [9 9];
+men3 = [1 2];
+analyzuj_vlastnosti(cit3, men3)
