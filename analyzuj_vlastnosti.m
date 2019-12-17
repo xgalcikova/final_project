fb4ea4badce85f1359544622364f1ea3b593c4c2
Lenka Galcikova <xgalcikova@stuba.sk>
2019-12-17 10:24:00 +0100

    major[mfiles]: added

---
+++ analyzuj_vlastnosti.m
@@ -0,0 +1,7 @@
+function analyzuj_vlastnosti(cit, men)
+    if length(cit)>length(men)
+        disp('System nie je fyzikalne realizovatelny.')
+    else
+        disp('System je fyzikalne realizovatelny.')
+    end        
+end
---
+++ testuj.m
@@ -0,0 +1,9 @@
+clc, clear, close all;
+% fyzikalna realizovatelnost
+cit1 = [9];
+men1 = [1 2 3];
+analyzuj_vlastnosti(cit1, men1)
+% fyzikalna nerealizovatelnost
+cit2 = [9 9 9];
+men2 = [1 2];
+analyzuj_vlastnosti(cit2, men2)
